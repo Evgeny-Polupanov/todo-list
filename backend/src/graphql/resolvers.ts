9aa454bea3644055b8bf4203586a7a5ce26433c4
evgeniypolupanov <evgeniypolupanov86@gmail.com>
2022-12-08 14:16:02 +0300

    add the login endpoint

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -3,9 +3,29 @@ import User from '../models/user'
 import Todo from '../models/todo'
 import bcrypt from 'bcrypt'
 import validator from 'validator'
+import jwt from 'jsonwebtoken'
 
 const resolvers: IResolvers = {
     Query: {
+        login: async (_, args) => {
+            const { email, password } = args.userInput
+
+            const error = new Error('Invalid credentials.')
+
+            const user = await User.findOne({ email })
+            if (!user) {
+                throw error
+            }
+            const isMatch = await bcrypt.compare(password, user.password)
+            if (!isMatch) {
+                throw error
+            }
+            const token = jwt.sign({
+                id: user._id.toString(),
+                email: user.email,
+            }, 'secret', { expiresIn: '1h' })
+            return { token, _id: user._id }
+        },
         getTodos: () => {
             return []
         },
@@ -14,21 +34,21 @@ const resolvers: IResolvers = {
         signup: async (_, args) => {
             const { email, name, password } = args.userInput
             if (!validator.isEmail(email)) {
-                throw 'The email is not valid.'
+                throw new Error('The email is not valid.')
             }
             if (validator.isEmpty(name)) {
-                throw 'The name should not be empty.'
+                throw new Error('The name should not be empty.')
             }
             if (
                 !validator.isAlphanumeric(password) ||
                 validator.isEmpty(password) ||
                 !validator.isLength(password, { min: 6 })
             ) {
-                throw 'The password is invalid.'
+                throw new Error('The password is invalid.')
             }
             const existingUser = await User.findOne({ email })
             if (existingUser) {
-                throw 'This email is already taken.'
+                throw new Error('This email is already taken.')
             }
             const hashedPassword = await bcrypt.hash(password, 12)
             const user = new User({ email, name, password: hashedPassword })
