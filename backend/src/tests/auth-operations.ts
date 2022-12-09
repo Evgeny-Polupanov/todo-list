e48f59b2369a8b617f7cfbc146e60c1111a7dc61
evgeniypolupanov <evgeniypolupanov86@gmail.com>
2022-12-09 15:15:33 +0300

    add a delete-user unit test

--- backend/src/tests/auth-operations.ts
+++ backend/src/tests/auth-operations.ts
@@ -6,6 +6,7 @@ import mongoose from 'mongoose'
 describe('Queries', () => {
     let supertest = request(app)
     let token: string
+    let userId: string
 
     before((done) => {
         const signupMutation = `
@@ -48,6 +49,7 @@ describe('Queries', () => {
                                     throw new Error(error)
                                 }
                                 token = JSON.parse(result.text).data.login.token
+                                userId = JSON.parse(result.text).data.login._id
                                 done()
                             })
                     })
@@ -64,7 +66,7 @@ describe('Queries', () => {
             .auth(token, { type: 'bearer' })
             .send({ query: deleteUserMutation })
             .expect(200)
-            .end((error, result) => {
+            .end((error) => {
                 if (error) {
                     throw new Error(error)
                 }
@@ -76,5 +78,64 @@ describe('Queries', () => {
 
     it('should return a token on login', () => {
         expect(token).to.exist
+    })
+
+    it('should delete user', (done) => {
+        const signupMutation = `
+            mutation {
+                signup(userInput: {
+                    email: "deleteTest@test.com"
+                    name: "Test"
+                    password: "testtest"
+                })
+            }
+        `
+        const loginQuery = `
+            {
+                login(userInput: {
+                    email: "deleteTest@test.com"
+                    password: "testtest"
+                }) {
+                    _id
+                    token
+                }
+            }
+        `
+        const deleteUserMutation = `
+            mutation {
+                deleteUser
+            }
+        `
+        let localToken
+        let localUserId
+        supertest.post('/graphql')
+            .send({ query: signupMutation })
+            .expect(200)
+            .end((error, result) => {
+                if (error) {
+                    throw new Error(error)
+                }
+                supertest.post('/graphql')
+                    .send({ query: loginQuery })
+                    .expect(200)
+                    .end((error, result) => {
+                        if (error) {
+                            throw new Error(error)
+                        }
+                        localToken = JSON.parse(result.text).data.login.token
+                        localUserId = JSON.parse(result.text).data.login._id
+                        supertest.post('/graphql')
+                            .auth(localToken, { type: 'bearer' })
+                            .send({ query: deleteUserMutation })
+                            .expect(200)
+                            .end((error, response) => {
+                                if (error) {
+                                    throw new Error(error)
+                                }
+                                expect(JSON.parse(response.text).data.deleteUser).to.be.true
+                                done()
+                            })
+                    })
+            })
     })
 })
